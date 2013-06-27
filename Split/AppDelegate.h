23b105ffadf586e7219da1f1fed7f44f5ea8c95d
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-06-27 00:56:15 -0500

    Initial Commit

---
+++ Split/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Split
+//
+//  Created by Jeffrey Camealy on 6/27/13.
+//  Copyright (c) 2013 orainteractive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
